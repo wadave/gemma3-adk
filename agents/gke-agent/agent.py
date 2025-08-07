d45145147e3b3c35aaeac4482b1414e3de27ec51
Dave Wang <wangdave@google.com>
2025-08-07 01:07:12 +0000

    format agent

--- agents/gke-agent/agent.py
+++ agents/gke-agent/agent.py
@@ -3,7 +3,7 @@ from google.adk.agents import LlmAgent
 from google.adk.models.lite_llm import LiteLlm
 from dotenv import load_dotenv
 
-load_dotenv()  
+load_dotenv()
 
 MODEL_NAME = os.getenv("MODEL_NAME")
 MODEL_VERSION = os.getenv("MODEL_VERSION")
@@ -12,11 +12,11 @@ api_base_url = "http://localhost:8000/v1"
 model_name_at_endpoint = f"hosted_vllm//gcs/{MODEL_NAME}/{MODEL_VERSION}"  #
 
 root_agent = LlmAgent(
+    name="root_agent",
     model=LiteLlm(
         model=model_name_at_endpoint,
         api_base=api_base_url,
     ),
-    name="root_agent",
     instruction=(
         """You are a helpful AI assistant designed to provide accurate and useful
         information."""
