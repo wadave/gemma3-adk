5f45533703e17f214c0514897105ec1d3c295bfd
Dave Wang <wangdave@google.com>
2025-08-07 01:05:44 +0000

    format agent

--- agents/gke-agent/agent.py
+++ agents/gke-agent/agent.py
@@ -1,30 +1,20 @@
-
 import os
 from google.adk.agents import LlmAgent
 from google.adk.models.lite_llm import LiteLlm
 from dotenv import load_dotenv
 
-load_dotenv()  # This loads the variables from .env into the environment
+load_dotenv()  
 
 MODEL_NAME = os.getenv("MODEL_NAME")
 MODEL_VERSION = os.getenv("MODEL_VERSION")
 
-
-
-# --- Example Agent using a model hosted on a vLLM endpoint ---
-
-# Endpoint URL provided by your vLLM deployment
 api_base_url = "http://localhost:8000/v1"
-
-# Model name as recognized by *your* vLLM endpoint configuration
-model_name_at_endpoint = f"hosted_vllm//gcs/{MODEL_NAME}/{MODEL_VERSION}" # Example from vllm_test.py
-
+model_name_at_endpoint = f"hosted_vllm//gcs/{MODEL_NAME}/{MODEL_VERSION}"  #
 
 root_agent = LlmAgent(
     model=LiteLlm(
         model=model_name_at_endpoint,
         api_base=api_base_url,
-   
     ),
     name="root_agent",
     instruction=(
@@ -32,5 +22,4 @@ root_agent = LlmAgent(
         information."""
     ),
     description="Answers questions about math problems.",
-   
 )
--- agents/vertexai-agent/agent.py
+++ agents/vertexai-agent/agent.py
@@ -1,34 +1,14 @@
 import os
 from google.adk.agents import LlmAgent
 from google.adk.models.lite_llm import LiteLlm
-import google.auth
 from dotenv import load_dotenv
 
-load_dotenv()  # This loads the variables from .env into the environment
+load_dotenv()  
 
 PROJECT_ID = os.getenv("PROJECT_ID")
 REGION = os.getenv("REGION")
-endpoint_id = os.getenv("VERTEX_AI_ENPOINT_ID")
-
-
-def _get_auth_headers() -> dict[str, str]:
-    """Gets the auth headers for the model garden endpoint."""
-    creds, _ = google.auth.default(
-        scopes=["https://www.googleapis.com/auth/cloud-platform"]
-    )
-    auth_req = google.auth.transport.requests.Request()
-    creds.refresh(auth_req)
-    return {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {creds.token}",
-    }
-
-
-auth_headers = _get_auth_headers()
-model = f"vertex_ai/openai/{endpoint_id}"  # fine tuned working with ADK
-
-
-print("The current model is: {model}")
+ENDPOINT_ID = os.getenv("VERTEX_AI_ENDPOINT_ID")
+model = f"vertex_ai/openai/{ENDPOINT_ID}"
 
 root_agent = LlmAgent(
     name="root_agent",
